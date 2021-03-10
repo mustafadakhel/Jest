fe5c5fabaf5c27cc359803868146cd1e50d55a3f
Mustafa <mstfdakhil@gmail.com>
2021-03-10 10:46:31 +0300

    Replace column and spacers with padding

--- app/src/main/java/com/martin/jokes/ui/main/layouts/JokeItem.kt
+++ app/src/main/java/com/martin/jokes/ui/main/layouts/JokeItem.kt
@@ -1,11 +1,12 @@
 package com.martin.jokes.ui.main.layouts
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -25,14 +26,10 @@ fun JokeItem(
             },
         elevation = 4.dp
     ) {
-        Column {
-            Spacer(modifier = Modifier.fillMaxWidth().height(12.dp))
-            Text(
-                text = joke.toString(),
-                modifier = Modifier.fillMaxWidth(0.9f).align(Alignment.CenterHorizontally),
-                textAlign = TextAlign.Center
-            )
-            Spacer(modifier = Modifier.fillMaxWidth().height(12.dp))
-        }
+        Text(
+            text = joke.toString(),
+            modifier = Modifier.fillMaxWidth().padding(16.dp, 16.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
